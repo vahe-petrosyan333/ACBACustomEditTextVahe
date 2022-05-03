76075b590bcedb514ed487091974d51f44166128
Vahe.Petrosyan <Vahe.Petrosyan@acba.am>
2022-05-03 12:10:48 +0400

    remove Developers code

---
+++ ACBACommon/src/main/java/com/example/acbacommon/ValidatorListener.kt
@@ -0,0 +1,5 @@
+package com.example.acbacommon
+
+interface ValidatorListener {
+    fun validate()
+}
--- app/src/main/java/com/example/acbacustomedittextvahe/MainActivity.kt
+++ app/src/main/java/com/example/acbacustomedittextvahe/MainActivity.kt
@@ -4,14 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.children
+import com.example.acbacommon.ValidatorListener
 import com.example.acbacustomedittextvahe.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+            buttonValidate.setOnClickListener {
+                isValid(root)
+            }
+        }
 
     }
 
+    fun isValid(root: View) {
+        if (root is ViewGroup) {
+            for (view in root.children) {
+                isValid(view)
+            }
+        } else if (root is ValidatorListener) {
+            root.validate()
+        }
+
+    }
 }
