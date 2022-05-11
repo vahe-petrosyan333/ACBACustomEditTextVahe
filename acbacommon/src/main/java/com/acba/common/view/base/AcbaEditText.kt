43ad192ad1cbc30c82ab6004a906ada3818b212c
vahe-petrosyan333 <mr.petrosyan92@mail.ru>
2022-05-12 00:25:14 +0400

    improved code from git comments

--- acbacommon/src/main/java/com/acba/common/view/base/AcbaEditText.kt
+++ acbacommon/src/main/java/com/acba/common/view/base/AcbaEditText.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import com.acba.common.R
-import com.acba.common.view.validatoredittext.Validator
+import com.acba.common.util.extentions.emailValidator
+import com.acba.common.util.extentions.passwordValidator
+import com.acba.common.util.extentions.regexValidator
+import com.acba.common.util.extentions.requiredValidator
 import com.acba.common.view.validatoredittext.Validable
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
@@ -12,22 +15,17 @@ import com.google.android.material.textfield.TextInputLayout
 
 class AcbaEditText : TextInputEditText, Validable {
 
-    val EMAIL_DEFAULT_ERROR by lazy { "Incorrect email" }
-    val PASSWORD_DEFAULT_ERROR by lazy { "Incorrect password" }
     companion object {
         const val UNDEFINED = -1
     }
-    lateinit var validator: Validator
 
+    private var isRequired: Boolean = false
     private var layoutId: Int = UNDEFINED
     private var textInputLayout: TextInputLayout? = null
-    private var errorMessage: String? = ""
-    private var mEmptyErrorMessage: String? = ""
+    private var errorText: String? = ""
     private var mMinLengthErrorMessage: String? = ""
-    private var mEmailErrorMessage: String? = ""
-    private var mPasswordErrorMessage: String? = ""
     private var regex: String? = ""
-    private var validatorType: Int = UNDEFINED
+    private var validationTag: Int = UNDEFINED
     private var minLength: Int = UNDEFINED
 
 
@@ -48,22 +46,19 @@ class AcbaEditText : TextInputEditText, Validable {
     }
 
     private fun init(context: Context, attributeSet: AttributeSet? = null) {
-        validator = Validator()
         val typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.AcbaEditText)
         try {
             layoutId = typedArray.getResourceId(R.styleable.AcbaEditText_layoutId, 0)
-            errorMessage = typedArray.getString(R.styleable.AcbaEditText_errorMessage)
+            errorText = typedArray.getString(R.styleable.AcbaEditText_errorMessage)
             regex = typedArray.getString(R.styleable.AcbaEditText_regex)
-            validatorType = typedArray.getInt(R.styleable.AcbaEditText_validator, 0)
+            validationTag = typedArray.getInt(R.styleable.AcbaEditText_validator, 0)
             minLength = typedArray.getInt(R.styleable.AcbaEditText_minLength, 0)
+            isRequired = typedArray.getBoolean(R.styleable.AcbaEditText_isRequired, false)
         } finally {
             typedArray.recycle()
         }
 
         try {
-            mEmptyErrorMessage = context.getString(context.resources.getIdentifier("empty_message", "string", context.packageName))
-            mEmailErrorMessage = context.getString(context.resources.getIdentifier("email_validation_error", "string", context.packageName))
-            mPasswordErrorMessage = context.getString(context.resources.getIdentifier("password_validation_error", "string", context.packageName))
             mMinLengthErrorMessage = String.format(context.getString(context.resources.getIdentifier("min_length_message", "string", context.packageName)), minLength)
         } catch (e: Exception) {
             e.printStackTrace()
@@ -75,33 +70,37 @@ class AcbaEditText : TextInputEditText, Validable {
         textInputLayout = (parent.parent as View).findViewById(layoutId)
     }
 
-    override fun validate() {
-        if (text.isNullOrEmpty()) {
-            showErrorWhenIsNotValid(mEmptyErrorMessage ?: "")
-            return
+    override fun validate(): Boolean {
+        val target = text.toString()
+        if (validationTag != UNDEFINED && !identifyValidationPatternAndValidate(validationTag, target)) {
+            if (isRequired || target.isNotEmpty()) {
+                (errorText ?: getValidationText(validationTag))?.let { showErrorState(it) }
+                return false
+            }
         }
-        if (!validator.checkMinLength(text.toString(), minLength)) {
-            showErrorWhenIsNotValid(mMinLengthErrorMessage ?: "")
-            return
+        showDefaultState()
+        return true
+    }
+
+    private fun identifyValidationPatternAndValidate(validationTag: Int, target: String): Boolean {
+        return when (validationTag) {
+            R.integer.validate_email -> target.emailValidator()
+            R.integer.validate_password -> target.passwordValidator()
+            R.integer.validate_required_field -> target.requiredValidator()
+            R.integer.validate_regex -> target.regexValidator(regex?.toRegex())
+            else -> false
         }
-        when (validatorType) {
-            1 -> {
-                if (!validator.isValidEmail(text.toString())) {
-                    showErrorWhenIsNotValid(mEmailErrorMessage ?: EMAIL_DEFAULT_ERROR)
-                } else textInputLayout?.error = null
-            }
-            2 -> {
-                if (!validator.isValidPassword(text.toString())) {
-                    showErrorWhenIsNotValid(mPasswordErrorMessage ?: PASSWORD_DEFAULT_ERROR)
-                } else textInputLayout?.error = null
+    }
 
-            }
-            3 -> {
-                if (!validator.validateFromRegex(regex, text.toString())) {
-                    showErrorWhenIsNotValid(errorMessage ?: "")
-                }
-            }
-
+    private fun getValidationText(validationTag: Int): String? {
+        return when (validationTag) {
+            R.integer.validate_email ->
+                context.getString(context.resources.getIdentifier("email_validation_error", "string", context.packageName))
+            R.integer.validate_password ->
+                context.getString(context.resources.getIdentifier("password_validation_error", "string", context.packageName))
+            R.integer.validate_required_field ->
+                context.getString(context.resources.getIdentifier("empty_message", "string", context.packageName))
+            else -> ""
         }
     }
 
@@ -109,11 +108,7 @@ class AcbaEditText : TextInputEditText, Validable {
         TODO("Not yet implemented")
     }
 
-    override fun showErrorState(messageRes: Int) {
-        TODO("Not yet implemented")
-    }
-
-    fun showErrorWhenIsNotValid(errorMessage: String) {
-        textInputLayout?.error = errorMessage
+    override fun showErrorState(errorText: String) {
+        textInputLayout?.error = errorText
     }
 }
--- acbacommon/src/main/java/com/acba/common/view/validatoredittext/Validable.kt
+++ acbacommon/src/main/java/com/acba/common/view/validatoredittext/Validable.kt
@@ -1,7 +1,7 @@
 package com.acba.common.view.validatoredittext
 
 interface Validable {
-    fun validate()
+    fun validate():Boolean
     fun showDefaultState()
-    fun showErrorState(messageRes: Int)
+    fun showErrorState(messageText: String)
 }
