f5c3cacf6e72fa4b547ee0c968e3e1ab657d56c4
vahe-petrosyan333 <mr.petrosyan92@mail.ru>
2022-05-22 01:45:03 +0400

    workManager

--- app/src/main/java/com/acba/acbadigital/MainActivity.kt
+++ app/src/main/java/com/acba/acbadigital/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.work.*
 import com.acba.acbadigital.databinding.ActivityMainBinding
 import com.acba.acbadigital.net.RetrofitInstance
 import com.acba.acbadigital.repositories.MainSharedRepositoryImpl
@@ -14,6 +17,9 @@ import com.acba.acbadigital.ui.MainViewModel
 import com.acba.common.view.validatoredittext.ValidatorListener
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "workmng"
+
     private var mLoading: View? = null
     private val viewModel: MainViewModel =
         MainViewModel(MainSharedRepositoryImpl(RetrofitInstance.getRequestService()))
@@ -23,10 +29,15 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater).apply {
             setContentView(root)
             buttonValidate.setOnClickListener {
-                isValid(root)
-                Log.d("DOUBLE_CLICK", "click")
+//                isValid(root)
+//                Log.d("DOUBLE_CLICK", "click")
+                setOneTimeWorkRequest()
             }
         }
+
+    }
+
+    private fun getRates() {
         mLoading = findViewById(R.id.loading_layout)
         viewModel.getRates(true)
         viewModel.ratesLiveData.observe(this) {
@@ -53,6 +64,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
+        const val KEY_COUNT = "key_count"
         val loaderLiveData: MutableLiveData<Boolean> = MutableLiveData()
+    }
+
+    private fun setOneTimeWorkRequest() {
+        val workManager = WorkManager.getInstance(applicationContext)
+        val data = Data.Builder()
+            .putInt(KEY_COUNT, 125)
+            .build()
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
+            .setConstraints(constraints)
+            .setInputData(data)
+            .build()
+        workManager.enqueue(oneTimeWorkRequest)
+        workManager.getWorkInfoByIdLiveData(oneTimeWorkRequest.id)
+            .observe(this) {
+                if (it.state.isFinished) {
+                    val data = it.outputData
+                    val message = data.getString(MyWorker.KEY_WORKER)
+                    Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+
+                }
+                Log.i(TAG, it.state.name)
+            }
     }
 }
