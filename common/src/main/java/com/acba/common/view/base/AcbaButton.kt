06c08778c9130049f0f02ac193c1e157c4dda812
Vahe.Petrosyan <Vahe.Petrosyan@acba.am>
2022-06-01 10:29:09 +0400

    improved modules

---
+++ common/src/main/java/com/acba/common/view/base/AcbaButton.kt
@@ -0,0 +1,41 @@
+package com.acba.common.view.base
+
+import android.content.Context
+import android.util.AttributeSet
+import com.acba.common.R
+import com.acba.common.util.listeners.CustomClickListener
+import com.google.android.material.button.MaterialButton
+
+
+class AcbaButton : MaterialButton {
+
+    private var isPreventDoubleClick: Boolean = true
+
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+        init(context, attributeSet)
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet, defStyledAttr: Int)
+            : super(context, attributeSet, defStyledAttr) {
+        init(context, attributeSet)
+    }
+
+    private fun init(context: Context, attributeSet: AttributeSet? = null) {
+        val typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.AcbaButton)
+        try {
+            isPreventDoubleClick =
+                typedArray.getBoolean(R.styleable.AcbaButton_isPreventDoubleClick, false)
+        } finally {
+            typedArray.recycle()
+        }
+    }
+
+    override fun setOnClickListener(listener: OnClickListener?) {
+        if (isPreventDoubleClick) super.setOnClickListener(CustomClickListener(listener))
+        else super.setOnClickListener(listener)
+    }
+}
---
+++ common/src/main/java/com/acba/common/view/base/AcbaToolBar.kt
@@ -0,0 +1,26 @@
+package com.acba.common.view.base
+
+import android.content.Context
+import android.util.AttributeSet
+import com.acba.common.R
+import com.google.android.material.appbar.MaterialToolbar
+
+class AcbaToolBar : MaterialToolbar {
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+        init(context, attributeSet)
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet, defStyledAttr: Int) : super(context, attributeSet) {
+        init(context, attributeSet)
+    }
+
+    private fun init(context: Context, attributeSet: AttributeSet? = null) {
+        val typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.AcbaToolBar)
+        typedArray.recycle()
+    }
+
+}
